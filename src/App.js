ae41adaf1487585691a807c9f8ef1f566ef7325f
Quiana Berry <berry.quiana@gmail.com>
2021-01-30 00:10:24 -0500

    trial1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import FetchUsers from './FetchUsers'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1 className="text-center bg-gray-900 text-white text-4xl pt-5 lg:text-6xl">
+        Random User Application
+      </h1>
+      <FetchUsers />
+    </>
+  )
 }
 
-export default App;
+export default App
